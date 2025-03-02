c10cdc11b4426c648677c00c16d5c4c92bbbf091
vincenzo.battisti <v.battisti@gruppoprime.it>
2025-03-02 09:32:25 +0100

    Aggiunto bot di monitoraggio upd

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 import os
 
 # Ottieni le variabili d'ambiente da Railway
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-URL = os.getenv("URL")
+TELEGRAM_BOT_TOKEN = "7616181870:AAEAVHZbP8vMgOam3ZoiTu5Acnq-DBB4FTw"
+TELEGRAM_CHAT_ID = "214186478"
+URL = "https://www.amazon.it/dp/B0DK93ZQPC/"
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
